b5fc2d5a59049f95bfb45cf57355a465906f0c0b
homeUser <homeEmail>
2016-12-18 15:46:06 +0800

    allow script call webmethod

--- ServicesPoint/ServicesPoint/CodecServiceM.asmx.cs
+++ ServicesPoint/ServicesPoint/CodecServiceM.asmx.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Services;
 
 namespace ServicesPoint {
+
     /// <summary>
     ///CodecServiceM 的摘要描述
     /// </summary>
@@ -12,7 +13,7 @@ namespace ServicesPoint {
     [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
     [System.ComponentModel.ToolboxItem(false)]
     // 若要允許使用 ASP.NET AJAX 從指令碼呼叫此 Web 服務，請取消註解下列一行。
-    // [System.Web.Script.Services.ScriptService]
+    [System.Web.Script.Services.ScriptService]
     public class CodecServiceM : System.Web.Services.WebService {
 
         [WebMethod]
